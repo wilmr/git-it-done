dcc46d218a2189c9fd6287a146a19fa8722c5589
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-08-04 18:04:07 -0700

    repos with more than 30 issues will include a link to github

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 var getRepoIssues = function(repo) {
@@ -11,6 +12,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
             // pass response data to dom function
                 displayIssues(data);
+                // check if api has paginatedd issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("there was a problem with your request")
@@ -54,4 +59,16 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("wilmr/run-buddy");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target","_blank");
+
+    // append to wwarning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
